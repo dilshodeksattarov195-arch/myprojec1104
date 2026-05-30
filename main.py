470f19dcc832d1d0152d672993cfb398696aaccf
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-30 19:29:16 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class databasePetchWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = databasePetchWorker()
+    print("Worker engine initialized.")
